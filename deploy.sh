0c4921d5a3224598158574f68033dc451b7e5432
Daniel Sarmiento <danielsarmientoenterprise@gmail.com>
2022-07-31 16:28:56 -0500

    Cambios y deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "Cambios y deploy"
 git push origin master
-ssh backsoul@ssh.backsoul.xyz "cd /var/docker-apps/groot && sudo sh update_branch.sh"
+ssh root@ssh.backsoul.dev "cd /var/docker-apps/groot && sudo sh update_branch.sh"
